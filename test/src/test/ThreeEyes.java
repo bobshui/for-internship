26c96c4fb8dabbe0365f72ae0aa6879391b40b5c
bobshui <szw199408@sina.com>
2017-03-16 12:02:23 +0800

    first version

---
+++ test/src/test/ThreeEyes.java
@@ -0,0 +1,12 @@
+package test;
+
+public class ThreeEyes {
+	public static void main(String[] args){
+		//获取两个数的最大值
+		int x = 100;
+		int y = 200;
+		int z = (x > y) ? x : y;
+		System.out.println("max： "+z);
+		
+	}
+}
